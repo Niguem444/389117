08807dc17837ccd90333de8f39c9b97adcd7a248
Ninguem444 <santosjoaoantonio367@gmail.com>
2025-07-03 11:30:05 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.getElementById('heart-container');
+
+let t = 0;
+
+function createHeart() {
+  if (t > Math.PI * 2) return;
+
+  const x = 16 * Math.pow(Math.sin(t), 3);
+  const y = 
+    13 * Math.cos(t) -
+    5 * Math.cos(2 * t) -
+    2 * Math.cos(3 * t) -
+    Math.cos(4 * t);
+
+  const heart = document.createElement('div');
+  heart.className = 'heart';
+  heart.style.left = `${x * 14 + 230}px`;
+  heart.style.top = `${-y * 14 + 250}px`; // inverte y para posicionar corretamente
+
+  container.appendChild(heart);
+
+  t += 0.1;
+  setTimeout(createHeart, 20); // menor tempo entre os corações
+}
+
+createHeart();
