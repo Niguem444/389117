45529d43f77471ee8e1ec940e2015313981d1875
Niguem444 <santosjoaoantonio367@gmail.com>
2025-07-03 14:04:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,7 @@ function createHeart() {
   if (t > Math.PI * 2) return;
 
   const x = 16 * Math.pow(Math.sin(t), 3);
-  const y =
-    13 * Math.cos(t) -
-    5 * Math.cos(2 * t) -
-    2 * Math.cos(3 * t) -
-    Math.cos(4 * t);
+  const y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
 
   const heart = document.createElement('div');
   heart.className = 'heart';
